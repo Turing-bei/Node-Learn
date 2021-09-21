0f4cebd0e8367a86f077867dab8db42e7dad7f9e
Turingbei <577390480@qq.com>
2021-09-22 00:32:45 +0800

    利用数据之间的关系

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -3,7 +3,11 @@ import { connection } from '../app/database/mysql';
  * 获取内容列表
  */
 export const getPosts = async () => {
-  const statement = `SElECT * FROM post`;
+  const statement = `SElECT
+  post.id,post.title,post.content,
+  JSON_OBJECT('id', user.id, 'name', user.name) as user
+  FROM post
+  LEFT JOIN user ON user.id = post.userId`;
   const [data] = await connection.promise().query(statement);
 
   return data;
