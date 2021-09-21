dd56ffa60ddfedd4431ba66e56a0237119b4c950
Turingbei <577390480@qq.com>
2021-09-22 00:27:20 +0800

    调取内容

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,18 +1,10 @@
+import { connection } from '../app/database/mysql';
 /**
  * 获取内容列表
  */
-export const getPosts = () => {
-  const data = [
-    {
-      content: '明月出天山，苍茫云海间',
-    },
-    {
-      content: '生当作人杰，死亦为鬼雄',
-    },
-    {
-      content: '天苍苍野茫茫，风吹草低见牛羊',
-    },
-  ];
+export const getPosts = async () => {
+  const statement = `SElECT * FROM post`;
+  const [data] = await connection.promise().query(statement);
 
   return data;
 };
