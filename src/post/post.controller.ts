6bb16f6fd10aba26e41b5dddf712ac47ce2c6bcd
Turingbei <577390480@qq.com>
2021-09-23 02:00:09 +0800

    测试更新内容

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,5 +1,5 @@
-import { Request, Response, NextFunction } from 'express';
-import { getPosts } from './post.service';
+import { Request, Response, NextFunction, request } from 'express';
+import { getPosts, createPost, updatePost } from './post.service';
 
 /**
  * 内容列表
@@ -12,6 +12,49 @@ export const index = async (
   try {
     const posts = await getPosts();
     response.send(posts);
+  } catch (error) {
+    next(error);
+  }
+};
+
+/**
+ * 创建内容
+ */
+export const store = async (
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) => {
+  // 准备数据
+  const { title, content } = request.body;
+
+  // 创建内容
+  try {
+    const data = await createPost({ title, content });
+    response.status(201).send(data);
+  } catch (error) {
+    next(error);
+  }
+};
+
+/**
+ * 更新内容
+ */
+export const updata = async (
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) => {
+  // 获取内容 ID
+  const { postId } = request.params;
+
+  // 准备数据
+  const { title, content } = request.body;
+
+  // 更新
+  try {
+    const data = await updatePost(parseInt(postId, 10), { title, content });
+    response.send(data);
   } catch (error) {
     next(error);
   }
