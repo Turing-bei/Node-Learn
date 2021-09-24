aa12e9d64cb20e0180a3578f69c44c49b7431206
Turingbei <577390480@qq.com>
2021-09-24 12:25:34 +0800

    验证用户密码是否匹配

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -18,9 +18,21 @@ export const createUser = async (user: UserModel) => {
 /**
  * 按用户名查找用户
  */
-export const getUserByName = async (name: string) => {
+interface GetUserOptios {
+  password?: boolean;
+}
+
+export const getUserByName = async (
+  name: string,
+  options: GetUserOptios = {},
+) => {
+  // 准备选项
+  const { password } = options;
+
   // 准备查询
-  const statement = `SELECT id, name FROM user WHERE name = ?`;
+  const statement = `SELECT id, name 
+  ${password ? ', password' : ''} 
+  FROM user WHERE name = ?`;
 
   // 执行查询
   const [data] = await connection.promise().query(statement, name);
