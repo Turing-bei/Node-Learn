2389b097307b49976bd7b91ea5c0c699a8f25efb
Turingbei <577390480@qq.com>
2021-09-26 01:06:26 +0800

    总结与整理

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Node 服务端应用开发
+
+## 生成密钥与公钥匙
+
+```
+cd config
+openssl
+genrsa -out private.key 4096
+rsa -in private.key -pubout -out public.key
+exit
+```
--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction, request } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import _ from 'lodash';
 import { getPosts, createPost, updatePost, deletePost } from './post.service';
 
@@ -28,10 +28,11 @@ export const store = async (
 ) => {
   // 准备数据
   const { title, content } = request.body;
+  const { id: userId } = request.user;
 
   // 创建内容
   try {
-    const data = await createPost({ title, content });
+    const data = await createPost({ title, content, userId });
     response.status(201).send(data);
   } catch (error) {
     next(error);
