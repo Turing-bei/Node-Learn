d61813a306034821799c96f8a7073382ae05307a
Turingbei <577390480@qq.com>
2021-10-09 15:58:46 +0800

    用户头像

--- src/app/app.middleware.ts
+++ src/app/app.middleware.ts
@@ -75,6 +75,10 @@ export const defaultErrorHandler = (
       statusCode = 400;
       message = '无法回复该条评论';
       break;
+    case 'FILE_TYPE_NOT_ACCEPT':
+      statusCode = 400;
+      message = '不能上传此类型的文件';
+      break;
     default:
       statusCode = 500;
       message = '服务器异常';
