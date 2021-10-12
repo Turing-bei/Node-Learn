657d5ddac5c714c08110d1281477afa78c899b17
Turingbei <577390480@qq.com>
2021-10-12 14:53:03 +0800

    资源接口

--- src/post/post.router.ts
+++ src/post/post.router.ts
@@ -2,13 +2,20 @@ import express from 'express';
 import * as postController from './post.controller';
 import { authGuard, accessControl } from '../auth/auth.middleware';
 import { sort, filter, paginate } from './post.middleware';
+import { POSTS_PER_PAGE } from '../app/app.config';
 
 const router = express.Router();
 
 /**
  * 内容列表
  */
-router.get('/posts', sort, filter, paginate, postController.index);
+router.get(
+  '/posts',
+  sort,
+  filter,
+  paginate(POSTS_PER_PAGE),
+  postController.index,
+);
 
 /**
  * 创建内容
@@ -54,6 +61,11 @@ router.delete(
   accessControl({ possession: true }),
   postController.destroyPostTag,
 );
+
+/**
+ *  单个内容
+ */
+router.get('/posts/:postId', postController.show);
 
 /**
  * 导出路由
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import { UserModel } from './user.model';
 import * as userService from './user.service';
+import _ from 'lodash';
 
 /**
  * 创建用户
@@ -17,6 +18,55 @@ export const store = async (
   try {
     const data = await userService.createUser({ name, password });
     response.status(201).send(data);
+  } catch (error) {
+    next(error);
+  }
+};
+
+/**
+ * 用户账户
+ */
+export const show = async (
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) => {
+  // 准备数据
+  const { userId } = request.params;
+
+  // 调取用户
+  try {
+    const user = await userService.getUserById(parseInt(userId, 10) as any);
+    if (!user) {
+      return next(new Error('USER_NOT_FOUND'));
+    }
+
+    // 做出响应
+    response.send(user);
+  } catch (error) {
+    next(error);
+  }
+};
+
+/**
+ * 更新用户
+ */
+export const update = async (
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) => {
+  // 准备数据
+  const { id } = request.user;
+  const userData = _.pick(request.body.update, ['name', 'password']);
+
+  // 更新用户
+
+  try {
+    const data = await userService.updateUser(id, userData);
+
+    // 做出响应
+    response.send(data);
   } catch (error) {
     next(error);
   }
